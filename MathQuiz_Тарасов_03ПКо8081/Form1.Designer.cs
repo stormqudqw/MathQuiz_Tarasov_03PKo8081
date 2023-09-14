a885cc6bb25be7fe3d24ba136e0854b2cb07115f
STORMqud <x7g100art@gmail.com>
2023-09-14 22:29:15 +0300

    1.0

--- MathQuiz_Тарасов_03ПКо8081/Form1.Designer.cs
+++ MathQuiz_Тарасов_03ПКо8081/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace MathQuiz_Тарасов_03ПКо8081
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.timeLabel = new System.Windows.Forms.Label();
             this.timeLable = new System.Windows.Forms.Label();
             this.plusLeftLabel = new System.Windows.Forms.Label();
@@ -52,6 +53,7 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.label12 = new System.Windows.Forms.Label();
             this.dividedLeftLabel = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
@@ -122,7 +124,9 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.sum.Location = new System.Drawing.Point(458, 62);
             this.sum.Name = "sum";
             this.sum.Size = new System.Drawing.Size(120, 35);
-            this.sum.TabIndex = 6;
+            this.sum.TabIndex = 1;
+            this.sum.Click += new System.EventHandler(this.answer_Enter);
+            this.sum.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // difference
             // 
@@ -130,7 +134,9 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.difference.Location = new System.Drawing.Point(458, 121);
             this.difference.Name = "difference";
             this.difference.Size = new System.Drawing.Size(120, 35);
-            this.difference.TabIndex = 11;
+            this.difference.TabIndex = 2;
+            this.difference.Click += new System.EventHandler(this.answer_Enter);
+            this.difference.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // label1
             // 
@@ -179,7 +185,9 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.product.Location = new System.Drawing.Point(458, 178);
             this.product.Name = "product";
             this.product.Size = new System.Drawing.Size(120, 35);
-            this.product.TabIndex = 16;
+            this.product.TabIndex = 3;
+            this.product.Click += new System.EventHandler(this.answer_Enter);
+            this.product.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // label6
             // 
@@ -227,7 +235,9 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.quotient.Location = new System.Drawing.Point(458, 238);
             this.quotient.Name = "quotient";
             this.quotient.Size = new System.Drawing.Size(120, 35);
-            this.quotient.TabIndex = 21;
+            this.quotient.TabIndex = 4;
+            this.quotient.Click += new System.EventHandler(this.answer_Enter);
+            this.quotient.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // label10
             // 
@@ -280,12 +290,18 @@ namespace MathQuiz_Тарасов_03ПКо8081
             this.startButton.TabIndex = 0;
             this.startButton.Text = "Начать тест";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.ClientSize = new System.Drawing.Size(602, 373);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.quotient);
             this.Controls.Add(this.label10);
@@ -345,6 +361,7 @@ namespace MathQuiz_Тарасов_03ПКо8081
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label dividedLeftLabel;
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
